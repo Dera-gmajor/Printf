935c8c869c38cff7c542a15d1b6b3a385543af84
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-22 17:17:32 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -6,9 +6,9 @@
  */
 int _printf(const char *format, ...)
 {
+	int v, w = 0;
 	va_list t;
-	int g, f = 0;
-	int *sn = &g;
+	int *sn = &w;
 	tag t[] = {
 		{'i', print_int}, {'c', print_char}, {'u', print_unsigned},
 		{'p', print_pointer}, {'s', print_strings}, {'d', print_int},
@@ -19,60 +19,78 @@ int _printf(const char *format, ...)
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+
 	va_start(t, format);
-	for (g = 0; format[g] != '\0'; g++)
+	for (v = 0; format[v] != '\0'; v++)
 	{
-		if (format[g] != '%')
+		if (format[v] != '%')
 		{
-			_putchar(format[g]);
+			_putchar(format[v]);
 			*sn += 1;
 		}
-		else if (format[g] == '%' && format[g + 1] != '%')
+		else if (format[v] == '%' && format[v + 1] != '%')
 		{
-			g++;
-
-			int v = g;
-			int a = 0;
-			sign s[] = {{'+'}, {' '}, {'#'}, {'\0'}};
-
-			while (d(format[v], s) != -1)
-			{
-				for (int m = 0; s[m]g != '\0'; m++)
-				{
-					if (format[v] == s[m].g)
-					{
-						if (format[v] == '+' && a < 2)
-							a = 1;
-						else if (format[v] == '#')
-							a = 2;
-						v++;
-						break;
-					}
-				}
-			}
-			int n = e(format[v], t);
-
-			if (n != -1)
-			{
-				if (s[m].g != '\0')
-					s[m].tg(a, s[m].g, n, t, sn);
-				t[n].tg(t, sn);
-				g = v;
-			}
-			else
-			{
-				_putchar('%');
-				(g)--;
-				*sn += 1;
-			}
+			v++;
+			tag_t(format, &v, x, sn, t);
 		}
-		else if (format[g] == '%' && format[g + 1] == '%')
+		else if (format[v] == '%' && format[v + 1])
 		{
-			g++;
-			_putchar(format[g]);
+			v++;
+			_putchar(format[v]);
 			*sn += 1;
 		}
 	}
 	va_end(t);
-	return (*sn);
+	return (w);
+}
+
+/**
+ * tag_t - checks for sign
+ * @format: string format
+ * @a: pointer to format
+ * @q: pointer to struct
+ * @sn: pointer to counter
+ * @t: pointer to arguements
+ */
+void tag_t(const char *format, int *a, struct tag *q, int *sn, va_list t)
+{
+	va_list u;
+	int m, n = 3;
+	int b = *a;
+	int h = 0;
+	sign s[] = {{'+'}, {' '}, {'#'}, {'\0'}};
+
+	va_copy(u, t);
+
+	while (d(format[b], s) != -1)
+	{
+		for (n = 0; s[n].g != '\0'; n++)
+		{
+			if (format[b] == s[n].g)
+			{
+				if (format[b] == '+' && h < 2)
+					h = 1;
+				else if (format[b] == '#')
+					h = 2;
+				b++;
+				break;
+			}
+		}
+	}
+	m = e(format[b], q);
+
+	if (m != -1)
+	{
+		if (s[n].g != '\0')
+			s[n].tg(h, s[n].g, m, u, sn);
+		q[m].tg(t, sn);
+		*a = b;
+	}
+	else
+	{
+		_putchar('%');
+		(*a)--;
+		*sn += 1;
+		return;
+	}
 }
