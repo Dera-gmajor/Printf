7110c09c5ff47b50e7296f0b9c609ef7f1731e12
Dera-gmajor <ibezimderarita@gmail.com>
2023-08-23 15:41:58 -0700

    printf

---
+++ _print_integer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_integer - prints an integer
+ * @t: points to the list of arguments
+ * @sn: pointer
+ * Return: 0
+ */
+int _print_integer(va_list t, int *sn)
+{
+	unsigned int n = va_arg(t, int);
+	_integer(n, sn);
+	return (0);
+}
